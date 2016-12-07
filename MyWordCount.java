23bcc61ac51f6669fce98242d72f34308c479614
Venkateswara Prasad Pandeti <vpandeti@cs.stonybrook.edu>
2016-12-06 21:29:54 -0500

    Initial commit
    + Updated README

---
+++ MyWordCount.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MyWordCount {
+	
+	private static final String CASE_SENSITIVE_KEY = "isCaseSensitive";
+
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			Configuration conf = context.getConfiguration();
+			boolean isCaseSensitive = conf.getBoolean(CASE_SENSITIVE_KEY, false);
+			String input = isCaseSensitive ? value.toString() : value
+					.toString().toLowerCase();
+			StringTokenizer itr = new StringTokenizer(input, ". ,\t()[]{}");
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.setBoolean(CASE_SENSITIVE_KEY, Boolean.valueOf(args[1]));
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(MyWordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setNumReduceTasks(Integer.valueOf(args[0]));
+		String path = args[2];
+		String files[] = null;
+		if(path.contains(",")) {
+			files = path.split(",");
+			for(String s: files) {
+				FileInputFormat.addInputPath(job, new Path(s));
+			}
+		} else {
+			FileInputFormat.addInputPath(job, new Path(path));
+		}
+		FileOutputFormat.setOutputPath(job, new Path(args[3]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Word-Count---Hadoop-MapReduce
-Calculates the word count in the given files using Hadoop MapReduce
+WordCount - Hadoop MapReduce
+----------------------------
+The program calculates the word count for the given input files
+
+1. Word is defined as separated by space, comma, period(.), tab(\t), parentheses(), brackets[], curly braces({}) characters.
+2. Program considers multiple input files in the given input directory
+3. Program accepts 4 positional arguments - Number of reducers, should the word be considered case-sensitive, input path and output path.
+
+Sample command to execute
+$ hadoop jar MyWordCount.jar 4 true /home/cloudera/input/big.txt /user/cloudera/outputWordCount
+
+Input path can be specified as hdfs path. For instance /user/cloudera/input/big.txt
+
+You can download big text file from here 
+http://norvig.com/big.txt
+http://www.gutenberg.org/
